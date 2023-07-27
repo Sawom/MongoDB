3f8366eeac0c717ccdf6b3d5104c93ceba2ed370
Abdur Rashid Sawom <asawom250@gmail.com>
2023-07-27 16:33:35 +0600

    Add files via upload

---
+++ naming_convention.js
@@ -0,0 +1,20 @@
+const dbCollection = client.db("exampleDatabase").collection("data");
+1 line e dea zay
+//naming convention
+
+database nam zodi users hoy
+
+app.get('/users')
+shob user pabo
+
+app.post('/users')
+1ta user post korbo
+
+app.get('/users/:id')
+id diye specific 1jn nibo
+
+app.put('/users/:id')
+1ta update korbo
+
+app.delete('/users/:id')
+1 jon delete korbo
