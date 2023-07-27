dd63294498603645f7e91714b56972cc48ed9a64
Abdur Rashid Sawom <asawom250@gmail.com>
2023-07-27 14:04:36 +0600

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const port = process.env.port || 5000;
+require('dotenv').config(); 
+// eta na dile dot env kaj kore na
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
 
 const app = express();
 
@@ -8,8 +11,26 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', async(req, res) => {
+// connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bsdjaxv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri , { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 } );
+
+async function run(){
+    try{
+        await client.connect();
+        console.log('connected successfully!');
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+// connection end
+
+app.get('/', (req, res) => {
     res.send('doctors portal running');
 })
 
-app.listen(port, ()=> console.log(`doctor portal server running at ${port}` ) )
+app.listen(port, ()=> {
+    console.log(`doctor portal server running at ${port}` );
+}) 
